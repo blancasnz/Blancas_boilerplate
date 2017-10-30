df10c80fbfc7e65cf3aa6dfe1aff7db8ced136e4
Blanca Sanchez <blancayllen@gmail.com>
2017-10-29 20:18:11 -0400

    added comments

--- client/reducers.js
+++ client/reducers.js
@@ -1,3 +1,7 @@
+
+// Define action types, action creators, and sub-reducers that your redux app will use to calculate the state. Use the connect function from react-redux to obtain slices of state and the dispatch method throughout your app
+
+
 function reducer(state = {}, action) {
   switch (action.type) {
     default:
